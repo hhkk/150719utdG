96f91729be3f9d8c5db748e0c113869576ef9709
hhkk <hank.khang@gmail.com>
2015-08-24 21:41:26 -0400

    sav

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -10,26 +10,31 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		// Home state routing
 		$stateProvider.
 			state('home', {
-				//templateUrl: 'modules/core/views/home.client.view.html'
 				url: '/',
+				//templateUrl: 'modules/core/views/home.client.view.html'
 				//templateUrl: 'modules/ustodos/views/view-ustodo.client.view.html'
+				//template: 'in core state home',
 				templateUrl: 'modules/ustodos/views/singlepage-ustodos.client.view.html',
 				controller: function ($scope) {
+					//alert ('start state '+$state.$current)
 					$scope.stateInfoHKstate='hihk';
 				}
 				//templateUrl: 'modules/core/views/home.client.view.html'
 			}).
 			state('hkhkstatecore1', {
+				// http://localhost:3000/#!/core/hkhkstatecore1
 				url: '/core/hkhkstatecore1',
 				template: 'in core state hkhkstatecore1 <a ui-sref="hkhkstatecore2">sref to hkhkstatecore2</a> <br> ' +
-				'<a href="/#!/core/hkhkstatecore2">href to #!/core/hkhkstatecore2</a>' +
-				'<a href="/#!/ustodos/hkhkstateustodos1">href to #!/ustodos/hkhkstateustodos1</a>',
+				'<a href="/#!/core/hkhkstatecore2">href to #!/core/hkhkstatecore2</a> <br> ' +
+				'<a href="/#!/ustodos/hkhkstateustodos1">href to #!/ustodos/hkhkstateustodos1</a> <br> ' +
+				'<a href="/#!/ustodos/hkhkstateustodos1">href to #!/ustodos/hkhkstateustodos1</a> <br> ',
 				controller: function ($scope, $state) {
 					alert ('start state '+$state.$current)
 					$scope.stateInfoHKstate='hihk';
 				}
 			}).
 			state('hkhkstatecore2', {
+				// http://localhost:3000/#!/core/hkhkstatecore2
 				url: '/core/hkhkstatecore2',
 				template: 'in core state hkhkstatecore2 <a ui-sref="hkhkstatecore1">sref to hkhkstatecore1</a>  <br> ' +
 				'<a href="/#!/core/hkhkstatecore1">href to #/core/hkhkstatecore1</a>',
